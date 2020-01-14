34f7e22065727adc43c9a8bdaced9f2be5b804de
Kien Nguyen <kien.nguyen3@ntq-solution.com.vn>
2020-01-14 16:31:17 +0700

    sdsd

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css';
+import axios from 'axios';
+import { Table, Divider, Tag,Button,Modal } from 'antd';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            data:[],
+            showModal:false,
+        }
+    }
+    toggleModal =()=>{
+        this.setState({
+            showModal:true,
+        })
+    }
+    handleOk = e => {
+        console.log(e);
+        this.setState({
+            showModal: false,
+        });
+    };
+
+    handleCancel = e => {
+        console.log(e);
+        this.setState({
+            showModal: false,
+        });
+    };
+    componentWillMount() {
+        axios.get('http://5d75320ad5d3ea001425b1ed.mockapi.io/products').then((response)=>{
+            this.setState({
+               data:response.data
+            });
+        })
+    }
+
+    render()
+{
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id',
+            render: text => <a>{text}</a>,
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Price',
+            dataIndex: 'price',
+            key: 'price',
+        },
+        {
+            title: 'Status',
+            key: 'status',
+            dataIndex: 'status',
+            render: text => <a>{!text ? 'true':'false'}</a>,
+        },
+    ];
+
+    const data = this.state.data;
+    return (
+        <div className="App">
+            <h1>Crud Example</h1>
+            <Button onClick={this.toggleModal} type="primary">Add </Button>
+            <Table columns={columns} dataSource={data} pagination={{ defaultPageSize: 5, showSizeChanger: true, pageSizeOptions: ['5', '10', '20']}} />
+            <Modal
+                title="Basic Modal"
+                visible={this.state.showModal}
+                onOk={this.handleOk}
+                onCancel={this.handleCancel}
+            >
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
+        </div>
+    );
+}
+
+}
+
+export default App;
